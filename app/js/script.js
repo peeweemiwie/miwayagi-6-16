9ef9b8cff909721f098ce9bb66167d766b585cc4
peeweemiwie <miwayagi@gmail.com>
2016-06-18 20:50:21 -0400

    Clean up css. Update nav function.

--- app/js/script.js
+++ app/js/script.js
@@ -2,23 +2,59 @@
 
   'use strict';
 
-  var body = $('body');
-  var mainNav = $('.main-nav');
+  var mainNav = $('nav.main-nav'),
+      mainNavAnchor = $('.main-nav-list a'),
+      navController = $('.main-nav button, header .headers'),
+      dataNav = 'data-nav';
 
+  var getDataState = function(el, attrKey){
+    var attrVal = $(el).attr(attrKey);
+    return attrVal;
+  };
 
-  $('button.menu, .main-nav-list, .headers').on('click', function(){
-    // body.attr('data-nav') === 'show' ? 
-    // body.attr('data-nav', 'hide') : 
-    // body.attr('data-nav', 'show');
-    mainNav.attr('data-nav') === 'show' ? 
-    mainNav.attr('data-nav', 'hide') : 
-    mainNav.attr('data-nav', 'show');
+  var changeDataState = function(el, attrKey, attrVal){
+    el.attr(attrKey, attrVal);
+  };
 
+  navController.on('click', function(){
+    var dataNavVal = getDataState(mainNav, dataNav);
+    if(dataNavVal === 'hide'){
+      changeDataState(mainNav, dataNav, 'show');
+    }else if(dataNavVal === 'show'){
+      changeDataState(mainNav, dataNav, 'hide');
+    }
   });
 
+  $(document).ready(function(){
+    var currHref = window.location.href,
+        mainNav = $('nav.main-nav'),
+        mainNavButton = $('nav.main-nav button'),
+        mainNavAnchor = $('.main-nav-list a'),
+        mainNavAnchorArray = [],
+        lastSegUlr = currHref.substr(currHref.lastIndexOf('/') + 1);
+
+    for(var i=0; i<mainNavAnchor.length; i++){
+      mainNavAnchorArray.push(mainNavAnchor[i].text);
+    }
+
+    // if nav link name (ie: #bio) is in current url, hide nav
+    if(mainNavAnchorArray.indexOf(lastSegUlr.substr(1)) > -1){
+      changeDataState(mainNav, dataNav, 'hide');
+    }
+
+    mainNavAnchor.on('click', function(){
+      changeDataState(mainNav, dataNav, 'hide');
+    });
+
+    mainNavButton.on('click', function(){
+      window.location.assign('#');
+    });
+  });
+
+  // contact section bubble animation.
   var makeBubble = function(num){
     for(var i=0; i < num; i++){
-      var span = $('<span>contact<span />').addClass('bubble');
+      var span = $('<span class="bubble">contact<span />');
       var randomNumber = (Math.floor(Math.random() * 100)) + 'px';
       $(span).width(randomNumber).height(randomNumber);
       $('.bubble-wrapper').append(span);
