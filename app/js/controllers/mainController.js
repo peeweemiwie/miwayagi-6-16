770131f5c1a15ad419555bdfd1c840fa1325da21
peeweemiwie <miwayagi@gmail.com>
2018-11-12 18:01:05 -0500

    Update information

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,10 +1,22 @@
 app.controller('mainController', ['$scope', function($scope) {
   $scope.nav = ['experience', 'portfolio', 'bio', 'contact'];
-  $scope.frontend = ['HTML5', 'CSS animation', 'SASS', 'Javascript', 'harp.js', 'angular.js', 'd3.js', 'node.js', 'highcharts', 'greenSock', 'jQuery', 'wordpress', 'A/B testing', 'Google Analytics', 'Omniture', 'git', 'agile'];
-    $scope.design = ['Sketch', 'Edge Animation', 'Photoshop', 'Illustrator', 'Flash'];
+  $scope.frontend = ['HTML5', 'CSS animation', 'SASS', 'Javascript', 'harp.js', 'angular.js', 'd3.js', 'greenSock', 'jQuery', 'wordpress', 'A/B testing', 'Google Analytics', 'git', 'agile'];
+    $scope.design = ['Sketch', 'Photoshop', 'Illustrator', 'Flash'];
     $scope.interest = ['accessibility', 'animation', 'data visualization', 'inclusive design', 'user-centered design'];
     $scope.works = [
-    {
+      {
+        'title': 'Design Technologist',
+        'time': {
+            'start': '11/2016',
+            'end': 'present'
+        },
+        'company': 'Cota Healthcare',
+        'detail': [
+          'Creating and managing style guide',
+          'UX/UI design as well as implementing design for Healthcare information management platforms',
+          'Ember.js, Angular.js, Google Material Design'
+        ]
+    },{
         'title': 'Design Technologist',
         'time': {
             'start': '6/2015',
@@ -24,7 +36,6 @@ app.controller('mainController', ['$scope', function($scope) {
             'end': '6/2015'
         },
         'company': 'New York Magazine',
-        
         'detail': [
           'Brands: ​New York Magazine, Vulture, The Cut, Bedford + Bowery, Science of Us',
           'Creating components from Photoshop wireframes using HTML, CSS and JavaScript in a Node.js environment',
@@ -37,34 +48,6 @@ app.controller('mainController', ['$scope', function($scope) {
           'prototype and page layout for a pop­up page which displayed Oscar real time results.',
           'utilization of Optimizely as an A/B testing tool, creating randomized experiments with multiple variants.',
           'creation of a child theme in WordPress for an NYU collaboration blog, Bedford + Bowery.'
-        ]
-    }, {
-        'title': 'HTML Developer',
-        'company': 'Freelance',
-        'time': {
-            'start': '3/2011',
-            'end': '4/2012'
-        },
-        'detail': [
-          'Clients: Standard & Poors, AKA NYC, Manhattan Marketing Ensemble, Tag Worldwide, Exposure New York, Mundocom - Publicis',
-          'Email campaigns and microsites for newly released electric appliances.',
-          'Flash ad banners.',
-          'Email campaigns and landing pages for an American global hospitality company to track promotions responses.',
-          'Internal site for a global leader in the beverage industry to provide their corporate guidelines.',
-          'Business-­to-­business newsletter templates for Microsoft Outlook, which are easily altered depending on users’ preference in subscription.'
-        ]
-    }, {
-        'title': 'HTML Developer',
-        'time': {
-            'start': '2/2009',
-            'end': '2/2011'
-        },
-        'company': 'Evolution Store',
-        
-        'detail': [
-          'HTML/CSS web­page development.',
-          'Developed splash web pages in HTML and CSS for e­-commerce site.',
-          'Coded, sent and tracked email campaigns using StreamSend.'
         ]
     }];
 
@@ -85,12 +68,10 @@ app.controller('mainController', ['$scope', function($scope) {
       {'url': 'p-lg_flash1.jpg', 'text': 'Flash animation start button - Flash'},
       {'url': 'p-lg_web2.jpg', 'text': 'Mockup for my client\'s website - Photoshop'},
       {'url': 'p-lg_email2.jpg', 'text': 'Mockup for an email campaign for my client - Photoshop'},
-      {'url': 'p-lg_email1.jpg', 'text': 'Mockup for an email campaign for my client - Photoshop'}, 
-      {'url': 'p-lg_web1.jpg', 'text': 'Mockup for my client\'s website - Photoshop'}, 
+      {'url': 'p-lg_email1.jpg', 'text': 'Mockup for an email campaign for my client - Photoshop'},
+      {'url': 'p-lg_web1.jpg', 'text': 'Mockup for my client\'s website - Photoshop'},
       {'url': 'p-lg_web4.jpg', 'text': 'Mockup for my client\'s website - Photoshop'},
       {'url': 'p-lg_ill1.jpg', 'text': 'Illustrator'},
-      {'url': 'p-fortune.jpg', 'text': 'Fortune cookie says... - Photoshop'},
-      {'url': 'p-fortune2.jpg', 'text': 'Fortune cookie says... - Photoshop'},
       {'url': 'p-icons.svg', 'text': 'Icons from my previous portfolio site - Sketch'}
     ];
 
@@ -113,7 +94,13 @@ app.controller('mainController', ['$scope', function($scope) {
       'title': 'CSS drawing and animation'
     }];
 
-    $scope.codepen = [{
+    $scope.codepen = [
+    {
+      'height': 619,
+      'slugHash': 'ZOPRpx',
+      'preview': false,
+      'title': 'CSS-animation Nubmers'
+    },{
       'height': 350,
       'slugHash': 'NrrEMW',
       'preview': true,
@@ -185,7 +172,6 @@ app.controller('mainController', ['$scope', function($scope) {
       'I also love cooking 🍳 and baking.🍰 I have immense love of sharing food I make with my friends and coworkers. *My Oatmeal cookies 🍪 are irresistible!',
       'When the weather is nice out, 🌞 I enjoy hiking,🚶 camping ⛺ and swimming. 🏊 There is nothing like being surrounded by nature.🍃',
       'Giving back to our community 👪 is another passion of mine. 👍 I have contributed to numerous organizations for people with cancer 💐 and underprivileged children.🚸',
-      'I reside in Roosevelt Island NYC 🚠 where I find it a very relaxing environment to live.🏡',
       'I adopted a cat 😺 recently. His name is Love who brightens my life.🐾',
       '*I bring my delicious baked goods 🍩 to work 2-3 times a month.✨'
     ];
